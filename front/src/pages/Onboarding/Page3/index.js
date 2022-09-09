801f1688844adbdc498c5845ae7bcaada0c04346
한윤석 <hys6078@naver.com>
2022-09-09 16:18:19 +0900

    feat: 🎸 page3 subtitle 빼고 제작 #1
    ✅ Closes: #1

--- front/src/pages/Onboarding/Page3/index.js
+++ front/src/pages/Onboarding/Page3/index.js
@@ -1,8 +1,16 @@
 import "./style.scss";
+import Dailyword from "assets/dailyword.jpg";
+
 export default function Page3() {
   return (
-    <div className="page-div">
-      <div className="background-div">asd</div>
+    <div className="page-color-div">
+      <div className="contents-div">
+        <h1 className="title-h1">
+          데일리 콘텐츠를 통해
+          <br /> <b>오늘의 단어</b>를 공부할 수 있습니다.sdasd
+        </h1>
+      </div>
+      <img src={Dailyword} alt="" className="dailyword-img" />
     </div>
   );
 }
