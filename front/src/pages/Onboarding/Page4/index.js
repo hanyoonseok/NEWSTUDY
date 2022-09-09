3bf05d1d2b3dbe4502a92f03fde7af4e7c63c469
한윤석 <hys6078@naver.com>
2022-09-09 21:39:25 +0900

    feat: 🎸 page 4 연결만 완료  #1
    ✅ Closes: #1

---
+++ front/src/pages/Onboarding/Page4/index.js
@@ -0,0 +1,18 @@
+import "./style.scss";
+
+export default function Page4() {
+  return (
+    <div className="page-div">
+      <div className="contents-div">
+        <h1 className="title-h1">
+          사용자별 <b>난이도</b>에 맞는 <br /> 기사를 제공합니다.
+        </h1>
+        <h3 className="subtitle-h3">
+          소식이 궁금한 나라가 있나요? <br />
+          지구 반대편에서 어떡 소식이 들려올지 궁금하신가요? <br />
+          뉴스터디의 지구본을 돌려가며 나라별 소식을 확인해보세요.
+        </h3>
+      </div>
+    </div>
+  );
+}
--- front/src/pages/Onboarding/index.js
+++ front/src/pages/Onboarding/index.js
@@ -4,6 +4,7 @@ import "./style.scss";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
+import Page4 from "./Page4";
 import Dots from "./Dots";
 import TEMP from "assets/temp.jpg";
 import Marker from "assets/marker.jpg";
@@ -23,13 +24,11 @@ export default function Onboarding() {
       const { deltaY } = e;
       const { scrollTop } = mainWrapperRef.current; // 스크롤 위쪽 끝부분 위치
       const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
-      console.log(scrollTop);
 
       if (deltaY > 0) {
         // 스크롤 내릴 때
         if (scrollTop >= 0 && scrollTop < pageHeight) {
           //현재 1페이지
-          console.log("현재 1페이지, down");
           mainWrapperRef.current.scrollTo({
             top: pageHeight + DIVIDER_HEIGHT,
             left: 0,
@@ -43,7 +42,6 @@ export default function Onboarding() {
           countrycard.current.style.opacity = 1;
         } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
           //현재 2페이지
-          console.log("현재 2페이지, down");
           mainWrapperRef.current.scrollTo({
             top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
             left: 0,
@@ -53,21 +51,26 @@ export default function Onboarding() {
           globe.current.style.opacity = "0";
           marker.current.style.opacity = 0;
           countrycard.current.style.opacity = 0;
-        } else {
+        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
           // 현재 3페이지
-          console.log("현재 3페이지, down");
           mainWrapperRef.current.scrollTo({
-            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
             left: 0,
             behavior: "smooth",
           });
           setScrollIdx(3);
+        } else {
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(4);
         }
       } else {
         // 스크롤 올릴 때
         if (scrollTop >= 0 && scrollTop < pageHeight) {
           //현재 1페이지
-          console.log("현재 1페이지, up");
           mainWrapperRef.current.scrollTo({
             top: 0,
             left: 0,
@@ -76,7 +79,6 @@ export default function Onboarding() {
           setScrollIdx(1);
         } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
           //현재 2페이지
-          console.log("현재 2페이지, up");
           mainWrapperRef.current.scrollTo({
             top: 0,
             left: 0,
@@ -88,9 +90,8 @@ export default function Onboarding() {
           globe.current.style.left = "50%";
           marker.current.style.opacity = 0;
           countrycard.current.style.opacity = 0;
-        } else {
+        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
           // 현재 3페이지
-          console.log("현재 3페이지, up");
           mainWrapperRef.current.scrollTo({
             top: pageHeight + DIVIDER_HEIGHT,
             left: 0,
@@ -100,6 +101,13 @@ export default function Onboarding() {
           globe.current.style.opacity = "1";
           marker.current.style.opacity = 1;
           countrycard.current.style.opacity = 1;
+        } else {
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(3);
         }
       }
     };
@@ -137,6 +145,10 @@ export default function Onboarding() {
 
       <div className="main-item">
         <Page3 />
+      </div>
+
+      <div className="main-item">
+        <Page4 />
       </div>
     </div>
   );
