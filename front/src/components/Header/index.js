f363344e617bbb6c10292e060500d337cdd5e1d8
한윤석 <hys6078@naver.com>
2022-09-13 17:54:36 +0900

    feat: 🎸 newsdetail 웹 뷰 작성중 #5
    ✅ Closes: #5

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,24 +1,10 @@
-import { useState, useEffect } from "react";
 import "./style.scss";
 
 export default function Header() {
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    const localStorageItem = localStorage.getItem("isLogin");
-    if (localStorageItem === null || localStorageItem === "false")
-      setIsLogin(false);
-    else setIsLogin(true);
-  }, []);
-
   return (
-    <>
-      {isLogin && (
-        <nav className="header-nav">
-          <input />
-          <button>login 되어잇음</button>
-        </nav>
-      )}
-    </>
+    <nav className="header-nav">
+      <input />
+      <button>login 되어잇음</button>
+    </nav>
   );
 }
--- front/src/pages/NewsDetail/index.js
+++ front/src/pages/NewsDetail/index.js
@@ -1,5 +1,12 @@
 import "./style.scss";
 
 export default function NewsDetail() {
-  return <div>s</div>;
+  return (
+    <div className="newsdetail-container">
+      <article className="back-btn-wrapper">
+        <button className="back-btn"></button>
+      </article>
+      <article className="newsdetail-content-article"></article>
+    </div>
+  );
 }
