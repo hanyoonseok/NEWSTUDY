5eb5bc0bf550172416b7a1bcc36e125ca3231888
Lee Hwayeon <snw4002@hanmail.net>
2022-09-13 13:43:10 +0900

    style: 💄 주석 삭제 #2
    ✅ Closes: #2

--- front/src/pages/Signup/index.js
+++ front/src/pages/Signup/index.js
@@ -1,7 +1,6 @@
 import "./style.scss";
 import Input from "./input";
 import { useState } from "react";
-import { useMediaQuery } from "react-responsive";
 
 export default function Signup() {
   const [activeId, setActiveId] = useState(0);
@@ -9,10 +8,6 @@ export default function Signup() {
   const onClickSwitchTab = (id) => {
     setActiveId(id);
   };
-
-  const isMobile = useMediaQuery({
-    query: "(max-width: 480px)",
-  });
 
   const tabContent = {
     0: (
@@ -42,8 +37,6 @@ export default function Signup() {
   return (
     <>
       <div className="background-div">
-        {/* {!isMobile ? (
-          <> */}
         <div className="left-div">
           <div className="logo-img">
             <img src={require("assets/logo.png")} alt="로고이미지"></img>
@@ -72,39 +65,6 @@ export default function Signup() {
           </div>
           <div className="content">{tabContent[activeId]}</div>
         </div>
-        {/* </>
-        ) : ( */}
-        {/* <div className="mobile-div">
-            <div className="top-div">
-              <div className="earth-img">
-                <img
-                  src={require("assets/earth-component.png")}
-                  alt="지구 컴포넌트 이미지"
-                ></img>
-              </div>
-              <div className="logo-img">
-                <img src={require("assets/logo.png")} alt="로고이미지"></img>
-              </div>
-            </div>
-            <div className="bottom-div">
-              <div className="tab">
-                <button
-                  className={`${activeId === 0 ? "active" : ""}`}
-                  onClick={() => onClickSwitchTab(0)}
-                >
-                  로그인
-                </button>
-                <button
-                  className={`${activeId === 1 ? "active" : ""}`}
-                  onClick={() => onClickSwitchTab(1)}
-                >
-                  회원가입
-                </button>
-              </div>
-              <div className="content">{tabContent[activeId]}</div>
-            </div>
-          </div>
-        )} */}
       </div>
     </>
   );
