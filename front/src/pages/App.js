f460a41eb79e596dd7dda165a9c3b0f7074a2d36
한윤석 <hys6078@naver.com>
2022-09-13 17:19:49 +0900

    feat: 🎸 기사 상세 페이지 세팅 #5
    ✅ Closes: #5

--- front/src/pages/App.js
+++ front/src/pages/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "pages/global.scss";
 import Onboarding from "pages/Onboarding";
+import NewsDetail from "pages/NewsDetail";
 import Layout from "components/Layout";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Onboarding />}></Route>
+          <Route path="/news/:id" element={<NewsDetail />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ front/src/pages/NewsDetail/index.js
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+export default function NewsDetail() {
+  return <div>s</div>;
+}
