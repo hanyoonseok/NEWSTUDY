604599cbe3839be98bac571afd67c49cc117f65e
김수빈 <yosopu0105@naver.com>
2022-09-14 21:47:37 +0900

    feat: 🎸 #9 시작중
    ✅ Closes: #9

--- front/src/pages/App.js
+++ front/src/pages/App.js
@@ -4,12 +4,15 @@ import "pages/global.scss";
 import Onboarding from "pages/Onboarding";
 import Layout from "components/Layout";
 
+import LevelTest from "pages/LevelTest";
+
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Onboarding />}></Route>
+          <Route path="/leveltest" element={<LevelTest />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ front/src/pages/LevelTest/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+function index(props) {
+  return (
+    <div>
+      <div className="leveltest-wrapper">
+        <span className=""></span>Level Test
+      </div>
+    </div>
+  );
+}
+
+export default index;
