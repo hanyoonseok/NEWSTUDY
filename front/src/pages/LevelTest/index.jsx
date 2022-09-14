311107db9768f72f253ff7da4cfe1315e3bc4fb5
김수빈 <yosopu0105@naver.com>
2022-09-15 08:48:53 +0900

    feat: 🎸 #9 레벨테스트 웹, 모바일 UI 작성
    ✅ Closes: #9

--- front/src/pages/LevelTest/index.jsx
+++ front/src/pages/LevelTest/index.jsx
@@ -1,14 +1,101 @@
-import React from "react";
+import { useState } from "react";
 import "./style.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function index(props) {
+import {
+  faCircleChevronRight,
+  faCircle,
+  faSquare,
+} from "@fortawesome/free-solid-svg-icons";
+function LevelTest(props) {
+  const leveltestWord = [
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "word",
+    "subin",
+    "subin",
+    "word",
+    "subin",
+    "subin",
+    "word",
+    "subin",
+    "word",
+    "subin",
+    "subin",
+    "word",
+    "word",
+  ];
+  const [pageState, setPageState] = useState(0);
+  const wordPage = {
+    0: [...leveltestWord].splice(0, 15),
+    1: [...leveltestWord].splice(15, 15),
+  };
   return (
     <div>
       <div className="leveltest-wrapper">
-        <span className=""></span>Level Test
+        <div className="leveltest-info">
+          <h1>
+            <span className="text-spotlight">LEVEL</span> TEST
+          </h1>
+          <div className="leveltest-desc">
+            알고있는 단어를 체크해주세요!
+            <img
+              className="check-img"
+              src={require("assets/check.png")}
+              alt="check"
+            ></img>
+          </div>
+        </div>
+        <div className="test-wrapper">
+          <div className="word-wrapper">
+            {wordPage[pageState].map((word, index) => (
+              <label className="word" key={`${index + pageState * 15}`}>
+                <input type="checkbox" name={index} />
+                <i>
+                  <FontAwesomeIcon icon={faSquare} className="check-icon" />
+                  <span className="test-word">{word}</span>
+                </i>
+              </label>
+            ))}
+          </div>
+          <div className="page-wrapper">
+            <div className={`${pageState === 0 ? "page1 active" : "page1"}`}>
+              <i>
+                <FontAwesomeIcon icon={faCircle} />
+              </i>
+            </div>
+            <div className={`${pageState === 1 ? "page2 active" : "page2"}`}>
+              <i onClick={() => setPageState(1)}>
+                <FontAwesomeIcon icon={faCircle} />
+              </i>
+            </div>
+          </div>
+        </div>
+        {pageState === 0 && (
+          <div className="next-btn">
+            <i onClick={() => setPageState(1)}>
+              <FontAwesomeIcon icon={faCircleChevronRight} />
+            </i>
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
-export default index;
+export default LevelTest;
