7e3434fd867192a2c7e3286ae6fc24409151dc5b
한윤석 <hys6078@naver.com>
2022-09-16 13:39:33 +0900

    refactor: 💡 #10 leveltest 페이지 모바일 뷰 그리드 수정
    ✅ Closes: #10

--- front/src/pages/LevelTest/index.jsx
+++ front/src/pages/LevelTest/index.jsx
@@ -43,74 +43,73 @@ function LevelTest(props) {
     "word",
   ];
   const [pageState, setPageState] = useState(0);
+
   const wordPage = {
     0: [...leveltestWord].splice(0, 15),
     1: [...leveltestWord].splice(15, 15),
   };
   return (
-    <div>
-      <div className="leveltest-wrapper">
-        <div className="leveltest-info">
-          <h1>
-            <span className="text-spotlight">LEVEL</span> TEST
-          </h1>
-          <div className="leveltest-desc">
-            알고있는 단어를 체크해주세요!
-            <img
-              className="check-img"
-              src={require("assets/check.png")}
-              alt="check"
-            ></img>
-          </div>
+    <div className="leveltest-wrapper">
+      <div className="leveltest-info">
+        <h1>
+          <span className="text-spotlight">LEVEL</span> TEST
+        </h1>
+        <div className="leveltest-desc">
+          알고있는 단어를 체크해주세요!
+          <img
+            className="check-img"
+            src={require("assets/check.png")}
+            alt="check"
+          ></img>
         </div>
-        <div className="test-wrapper">
-          <div className="word-wrapper">
-            {wordPage[pageState].map((word, index) => (
-              <label className="word" key={`${index + pageState * 15}`}>
-                <input type="checkbox" name={index} />
-                <i>
-                  <FontAwesomeIcon icon={faSquare} className="check-icon" />
-                  <span className="test-word">{word}</span>
-                </i>
-              </label>
-            ))}
-          </div>
-          <div className="page-wrapper">
-            <div className={`${pageState === 0 ? "page1 active" : "page1"}`}>
-              <i onClick={() => setPageState(0)}>
-                <FontAwesomeIcon icon={faCircle} />
+      </div>
+      <div className="test-wrapper">
+        <div className="word-wrapper">
+          {wordPage[pageState].map((word, index) => (
+            <label className="word" key={`${index + pageState * 15}`}>
+              <input type="checkbox" name={index} />
+              <i>
+                <FontAwesomeIcon icon={faSquare} className="check-icon" />
+                <span className="test-word">{word}</span>
               </i>
-            </div>
-            <div className={`${pageState === 1 ? "page2 active" : "page2"}`}>
-              <i onClick={() => setPageState(1)}>
-                <FontAwesomeIcon icon={faCircle} />
-              </i>
-            </div>
-          </div>
+            </label>
+          ))}
         </div>
-        {pageState === 0 && (
-          <div className="next-btn">
-            <i onClick={() => setPageState(1)}>
-              <FontAwesomeIcon icon={faCircleChevronRight} />
+        <div className="page-wrapper">
+          <div className={`${pageState === 0 ? "pagedot active" : "pagedot"}`}>
+            <i onClick={() => setPageState(0)}>
+              <FontAwesomeIcon icon={faCircle} />
             </i>
           </div>
-        )}
-        {pageState === 1 && (
-          <>
-            <div className="prev-btn">
-              <i onClick={() => setPageState(0)}>
-                <FontAwesomeIcon icon={faCircleChevronLeft} />
-              </i>
-            </div>
-            <button className="submit-btn">
-              제출
-              <i>
-                <FontAwesomeIcon icon={faChevronRight} />
-              </i>
-            </button>
-          </>
-        )}
+          <div className={`${pageState === 1 ? "pagedot active" : "pagedot"}`}>
+            <i onClick={() => setPageState(1)}>
+              <FontAwesomeIcon icon={faCircle} />
+            </i>
+          </div>
+        </div>
       </div>
+      {pageState === 0 && (
+        <div className="next-btn">
+          <i onClick={() => setPageState(1)}>
+            <FontAwesomeIcon icon={faCircleChevronRight} />
+          </i>
+        </div>
+      )}
+      {pageState === 1 && (
+        <div className="submit-container">
+          <div className="prev-btn">
+            <i onClick={() => setPageState(0)}>
+              <FontAwesomeIcon icon={faCircleChevronLeft} />
+            </i>
+          </div>
+          <button className="submit-btn">
+            제출
+            <i>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </i>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
