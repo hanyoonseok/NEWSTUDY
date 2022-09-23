b9b16bdbc106097db8adb827d19109654eec55fd
정수빈 <integerbin2@naver.com>
2022-09-23 22:37:52 +0900

    feat: 🎸 #16 user 정보 사용하도록 수정

--- back/newstudy/src/main/java/com/ssafy/newstudy/model/service/BadgeService.java
+++ back/newstudy/src/main/java/com/ssafy/newstudy/model/service/BadgeService.java
@@ -1,23 +1,25 @@
 package com.ssafy.newstudy.model.service;
 
+import com.ssafy.newstudy.model.dao.BadgeDao;
 import com.ssafy.newstudy.model.dto.BadgeRequest;
 import com.ssafy.newstudy.model.dto.BadgeResponse;
 import com.ssafy.newstudy.model.dto.UserTmp;
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
-public interface BadgeService {
-    /**
-     * 유저를 받아서 해당 유저의 배지 리스트를 돌려줌
-     * @param user
-     * @return 유저의 배지 리스트
-     */
-    List<BadgeResponse> getBadge(UserTmp user);
+@Service
+@RequiredArgsConstructor
+public class BadgeServiceImpl implements BadgeService{
+    private final BadgeDao badgeDao;
 
-    /**
-     * 유저와 배지넘버를 받아서 user-badge에 저장함
-     * @param badge
-     * @return 성공 row 수
-     */
-    int addBadge(BadgeRequest badge);
+    @Override
+    public List<BadgeResponse> getBadge(UserTmp user) {
+        return badgeDao.getBadge(user.getU_id());
+    }
+
+    @Override
+    public int addBadge(BadgeRequest badge) {
+        return badgeDao.addBadge(badge);
+    }
 }
