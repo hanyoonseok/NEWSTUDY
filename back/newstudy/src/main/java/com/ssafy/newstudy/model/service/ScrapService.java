5d20420f0e45a804f6fe0937aefd851a885685d6
정수빈 <integerbin2@naver.com>
2022-09-25 17:06:30 +0900

    feat: 🎸 #24 scrap service 추가

--- back/newstudy/src/main/java/com/ssafy/newstudy/model/service/ScrapService.java
+++ back/newstudy/src/main/java/com/ssafy/newstudy/model/service/ScrapService.java
@@ -1,2 +1,42 @@
-package com.ssafy.newstudy.model.service;public class ScrapService {
+package com.ssafy.newstudy.model.service;
+
+import com.ssafy.newstudy.model.dao.ScrapDao;
+import com.ssafy.newstudy.model.dto.ScrapRequestDto;
+import com.ssafy.newstudy.model.dto.ScrapResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScrapService {
+    private final ScrapDao scrapDao;
+
+    /**
+     * 유저를 받아서 해당 유저의 스크랩 뉴스 리스트를 돌려줌
+     * @param u_id
+     * @return 유저의 스크랩 리스트
+     */
+    public List<ScrapResponseDto> getScrap(Integer u_id) {
+        return scrapDao.selectScrapList(u_id);
+    }
+
+    /**
+     * 유저 뉴스 스크랩을 추가함
+     * @param scrapRequestDto
+     * @return 성공 row 수
+     */
+    public int addScrap(ScrapRequestDto scrapRequestDto) {
+        return scrapDao.insertScrap(scrapRequestDto);
+    }
+
+    /**
+     * 유저 뉴스 스크랩을 삭제함
+     * @param scrapRequestDto
+     * @return 성공 row 수
+     */
+    public int deleteScrap(ScrapRequestDto scrapRequestDto) {
+        return scrapDao.deleteScrap(scrapRequestDto);
+    }
 }
