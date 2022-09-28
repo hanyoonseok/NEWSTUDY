131a0cdb82296f703ef3ea5efd5f618eec55797b
정수빈 <integerbin2@naver.com>
2022-09-28 14:59:38 +0900

    fix: 🐛 #37 news list -> category id 범위로  찾도록 수정 + cross game 수정

--- back/newstudy/src/main/java/com/ssafy/newstudy/controller/NewsController.java
+++ back/newstudy/src/main/java/com/ssafy/newstudy/controller/NewsController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -43,14 +44,18 @@ public class NewsController {
     }
 
     @PostMapping()
-    @ApiOperation(value = "뉴스 리스트", notes = "뉴스 리스트를 가져온다 => level(없으면 로그인한 유저의 lv), categoryid(없거나 0이면 전체), search, page(필수!) 사용 가능 //하둡으로 검색 바꿀 예정")
+    @ApiOperation(value = "뉴스 리스트", notes = "뉴스 리스트(newsList)를 담은 HashMap을 가져온다  \n " +
+            "(newsList 로 가져올 수 있음, totalCnt로 총 검색 결과 카운트 가져오기) \n" +
+            "=> level(없으면 로그인한 유저의 lv), startcategoryid, endcategory (둘이 세트. 둘 다 없거나 둘 다 0이면 전체), \n" +
+            "titlekeyword, categorykeyword (타이틀만 검색 -> titlekeyword, 카테고리만 -> categorykeyword, 둘 다 하려면 양쪽에) \n" +
+            ", page(필수!) 사용 가능 //하둡으로 검색 바꿀 예정")
     @ApiResponses({
             @ApiResponse(code = 200, message="성공", response = List.class),
             @ApiResponse(code = 401, message="로그인정보 없음"),
             @ApiResponse(code = 500, message="서버오류")
     })
-    public ResponseEntity<List<NewsResponseDto>> getNewsList(@ApiParam(value = "로그인된 유저 정보", required = true) @RequestHeader("Authorization") String bearerToken,
-                                                             @RequestBody NewsRequestDto newsRequestDto){
+    public ResponseEntity<HashMap> getNewsList(@ApiParam(value = "로그인된 유저 정보", required = true) @RequestHeader("Authorization") String bearerToken,
+                                               @RequestBody NewsRequestDto newsRequestDto){
         //request에 레벨 범위가 없다면 본인레벨 start, end
         if(newsRequestDto == null || newsRequestDto.getStartlevel() == null) {
             UserDto user = userService.getUserByUid(userService.getUidFromBearerToken(bearerToken));
@@ -62,9 +67,19 @@ public class NewsController {
         }
 
         //레벨 범위 있으면 그대로
-        //하둡으로 대체할 예정
         List<NewsResponseDto> responseArray = newsService.getNewsList(newsRequestDto);
-        return new ResponseEntity<List<NewsResponseDto>>(responseArray, HttpStatus.OK);
+        //카운트 세기
+        Integer total_cnt = 0;
+        if(newsRequestDto.getTotal_cnt() == null) {
+            total_cnt = newsService.getSearchListTotalCnt(newsRequestDto);
+        }else
+            total_cnt = newsRequestDto.getTotal_cnt();
+
+        //Map
+        HashMap map = new HashMap();
+        map.put("newsList", responseArray);
+        map.put("totalCnt", total_cnt);
+        return new ResponseEntity<HashMap>(map, HttpStatus.OK);
     }
 
     @GetMapping("/keyword/{n_id}")
--- back/newstudy/src/main/java/com/ssafy/newstudy/model/dto/NewsResponseDto.java
+++ back/newstudy/src/main/java/com/ssafy/newstudy/model/dto/NewsResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
