363cc4a5fd2ae08303a340f5be45eab26f48c137
Lee Hwayeon <snw4002@hanmail.net>
2022-09-29 22:17:20 +0900

    feat: 🎸 #44 마이페이지 단어 뜻 한글 나오게 수정
    ✅ Closes: #44

--- front/src/components/FilterModal/index.jsx
+++ front/src/components/FilterModal/index.jsx
@@ -1,9 +1,33 @@
 import "./style.scss";
 import { categoryFilter } from "constants";
 import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function FilterModal({ closeHandler }) {
+  const user = useSelector((state) => state.user);
   const mainCategory = Object.keys(categoryFilter);
+  const [category, setCategory] = useState([]);
+  // const []
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("/category", {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          setCategory(res.data);
+        });
+    };
+    fetchData();
+    return () => {};
+  }, []);
 
   return (
     <div className="outside-area">
@@ -11,11 +35,22 @@ export default function FilterModal({ closeHandler }) {
         <h1 className="filter-modal-title">Category</h1>
         <div className="category-area">
           <article className="main-category-container">
-            {mainCategory.map((e, i) => (
-              <div className="main-category-btn" key={i}>
-                {e}
-              </div>
-            ))}
+            {category.length > 0 &&
+              category.map((e, i) => (
+                <label className="main-category-btn" key={i}>
+                  <input
+                    type="checkbox"
+                    name={i}
+                    // checked={
+                    //   checkedList.includes(index + pageState * 15) ? true : false
+                    // }
+                    // onChange={(e) =>
+                    //   addCheckList(e.target.checked, index + pageState * 15)
+                    // }
+                  />
+                  {e.main}
+                </label>
+              ))}
           </article>
           <article className="sub-category-container"></article>
         </div>
