18c77b058be98444bccb3a37836a02d2fffdb61b
hys6078 <hys6078@naver.com>
2022-10-01 00:44:11 +0900

    refactor: 💡 #30 낱말퍼즐 단어장 추가 로직 수정
    ✅ Closes: #30

--- front/src/pages/CrossWord/WordModal/index.jsx
+++ front/src/pages/CrossWord/WordModal/index.jsx
@@ -24,7 +24,7 @@ export default function WordModal({ wordInfo, setSelectedWord }) {
         Authorization: `Bearer ${userState.accessToken}`,
       },
     };
-    await axios.post("/vocaburary", wordInfo.eng, headers).then(() => {
+    await axios.post("/vocaburary", { eng: wordInfo.eng }, headers).then(() => {
       setIsModalOpen(true);
       setTimeout(() => {
         setIsModalOpen(false);
